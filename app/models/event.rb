3c3385e0713ef20855dc9ef0c8cf1f29807dc5b0
Yuki Yamashita <yyama0704@gmail.com>
2017-03-19 04:13:43 +0900

    add dependent: :destroy to event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,10 +1,10 @@
 class Event < ApplicationRecord
-	has_many :event_datetimes
-	has_many :event_docs
-	has_many :event_infos
-	has_one :event_long_desc
-	has_one :event_short_desc
-	has_one :event_schedule
+	has_many :event_datetimes, dependent: :destroy
+	has_many :event_docs, dependent: :destroy
+	has_many :event_infos, dependent: :destroy
+	has_one :event_long_desc, dependent: :destroy
+	has_one :event_short_desc, dependent: :destroy
+	has_one :event_schedule, dependent: :destroy
 	has_and_belongs_to_many :members
 
 	scope :in_wanted, -> { where("start_date > ?", Date.today) }
