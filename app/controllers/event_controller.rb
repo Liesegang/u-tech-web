536a36fd884f4843b9825939851949dd3e2d8792
Yuki Yamashita <yyama0704@gmail.com>
2017-03-19 02:47:09 +0900

    add event tips to event index page

--- app/controllers/event_controller.rb
+++ app/controllers/event_controller.rb
@@ -1,8 +1,8 @@
 class EventController < ApplicationController
   def index
-  	@wanteds = Events.in_wanted.sort_soon.take(20)
-  	@sessions = Events.in_session.sort_soon.take(20)
-  	@ends = Events.in_end.sort_soon.take(20)
+  	@wanteds = Event.in_wanted.sort_soon.take(20)
+  	@sessions = Event.in_session.sort_soon.take(20)
+  	@ends = Event.in_end.sort_soon.take(20)
   end
 
   def show
