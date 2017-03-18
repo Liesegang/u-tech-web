f67b6351b30a02935d41ce35706b8f110ecc1a8a
Yuki Yamashita <yyama0704@gmail.com>
2017-03-19 01:38:05 +0900

    add date-range scopes to event

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,4 +6,10 @@ class Event < ApplicationRecord
 	has_one :event_short_desc
 	has_one :event_schedule
 	has_and_belongs_to_many :members
+
+	scope :in_wanted, -> { where("start_date > ?", Date.today) }
+	scope :in_session, -> { where("(start_date <= ?) AND (end_date >= ?)", Date.today, Date.tiday) }
+	scope :in_end, -> { where("end_date < ?", Date.today) }
+
+	scope :sort_soon, ->　{ order(start_date: :asc) }
 end
