6699830d64fccd2469099dc0f7451f4b69d4a142
Yuki Yamashita <yyama0704@gmail.com>
2017-03-19 02:20:42 +0900

    add rails source code reading event to event table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Event.create(name: "Rails Code Reading", year: 2017, kind: "春休みゼミ", start_date: '2017-02-07', end_date: '2017-03-28', image: "rails_code_reading.png")
