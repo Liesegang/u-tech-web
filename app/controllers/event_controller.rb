abbc347fc2dd8b1fedab3649f6e8e9a001c0669b
Yuki Yamashita <yyama0704@gmail.com>
2017-03-19 01:42:50 +0900

    add local variables to event index

--- app/controllers/event_controller.rb
+++ app/controllers/event_controller.rb
@@ -1,5 +1,8 @@
 class EventController < ApplicationController
   def index
+  	@wanteds = Events.in_wanted.sort_soon.take(20)
+  	@sessions = Events.in_session.sort_soon.take(20)
+  	@ends = Events.in_end.sort_soon.take(20)
   end
 
   def show
