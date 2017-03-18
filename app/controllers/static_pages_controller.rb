76d51400c09038a10108d3973a54242809044941
Yuki Yamashita <yyama0704@gmail.com>
2017-03-18 17:44:24 +0900

    add static pages(home, about, welcome, event)

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,20 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+
+  def welcome
+  end
+
+  def event
+  end
+
+  def join
+  end
+
+  def contact
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+
+  root to: 'static_pages#home'
+
+  get '/about', to: 'static_pages#about'
+  get '/welcome', to: 'static_pages#welcome'
+  get '/event', to: 'static_pages#event'
+  get '/join', to: 'static_pages#join'
+  get '/contact', to: 'static_pages#contact'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
