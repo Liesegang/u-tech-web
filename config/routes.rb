e9c6774aa7d87b29a4e8c944d34aa6d05595089f
Yuki Yamashita <yyama0704@gmail.com>
2017-03-20 18:24:40 +0900

    create session model

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,20 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+
   root to: 'static_pages#home'
 
   resources :events, only: [:index, :show]
   resources :members
 
-  get '/about', to: 'static_pages#about'
-  get '/welcome', to: 'static_pages#welcome'
-  get '/join', to: 'static_pages#join'
-  get '/contact', to: 'static_pages#contact'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destro'
+
+  get '/about'   => 'static_pages#about'
+  get '/welcome' => 'static_pages#welcome'
+  get '/join'    => 'members#new'
+  get '/contact' => 'static_pages#contact'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
