7ad033db479e0192023d95e5c18caa65c97a9082
Yuki Yamashita <yyama0704@gmail.com>
2017-03-22 17:33:47 +0900

    add login and logout button

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
 
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
-  delete 'logout'  => 'sessions#destro'
+  delete 'logout'  => 'sessions#destroy'
 
   get '/about'   => 'static_pages#about'
   get '/welcome' => 'static_pages#welcome'
