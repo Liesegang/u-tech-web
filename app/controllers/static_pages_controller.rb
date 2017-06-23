909737423e866f6d449d7db9dfd5a5c9da556a71
Shunsuke Sato <wisteria0410ss@gmail.com>
2017-06-23 13:56:19 +0900

    Remove out-of-date welcome infomation

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,12 +1,13 @@
 class StaticPagesController < ApplicationController
   def home
-    @wanteds = Event.in_wanted.sort_soon.take(20)
+      @wanteds = Event.in_wanted.sort_soon.take(20)
   end
 
   def about
   end
 
   def welcome
+      render 'welcome_offseason'
   end
 
   def event
