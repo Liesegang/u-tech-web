c9b1215efa0db270e5d100026eb4a91eacb0b104
Shunsuke Sato <wisteria0410ss@gmail.com>
2017-06-23 14:10:55 +0900

    Correct the indent

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,22 +1,21 @@
 class StaticPagesController < ApplicationController
-  def home
-      @wanteds = Event.in_wanted.sort_soon.take(20)
-  end
+    def home
+        @wanteds = Event.in_wanted.sort_soon.take(20)
+    end
+    
+    def about
+    end
 
-  def about
-  end
+    def welcome
+        render 'welcome_offseason'
+    end
 
-  def welcome
-      render 'welcome_offseason'
-  end
+    def event
+    end
 
-  def event
-  end
+    def join
+    end
 
-  def join
-  end
-
-  def contact
-  end
-
+    def contact
+    end
 end
